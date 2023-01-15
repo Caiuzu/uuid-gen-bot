84dbcfa17751192a8a5e60d2c116e87fdefeb21b
Caio Souza <caio.7s@outlook.com>
2023-01-15 17:39:06 -0300

    refactor: Importanto e utilizando implementação de geração de uuid.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
-const uuid = require('uuid');
+const { generateUuid1, generateUuid4, generateMultipleUuid4 } = require('./src/uuid');
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/uuid1/, (msg) => {
   const chatId = msg.chat.id;
-  const uuid1 = uuid.v1();
-  bot.sendMessage(chatId, uuid1, {parse_mode: "Markdown"});
+  const uuid1 = generateUuid1();
+  bot.sendMessage(chatId, uuid1, { parse_mode: "Markdown" });
 });
 
 bot.onText(/\/uuid4/, (msg) => {
   const chatId = msg.chat.id;
-  const uuid4 = uuid.v4();
-  bot.sendMessage(chatId, uuid4, {parse_mode: "Markdown"});
+  const uuid4 = generateUuid4();
+  bot.sendMessage(chatId, uuid4, { parse_mode: "Markdown" });
 });
 
 bot.onText(/\/uuid4 (\d+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const amount = match[1];
-  let uuids = "";
-  for (let i = 0; i < amount; i++) {
-    uuids += uuid.v4() + "\n";
-  }
-  bot.sendMessage(chatId, uuids, {parse_mode: "Markdown"});
+  const uuids = generateMultipleUuid4(amount);
+  bot.sendMessage(chatId, uuids, { parse_mode: "Markdown" });
 });
