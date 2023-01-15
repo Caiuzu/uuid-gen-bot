5025ff94c400f1b086db9d42946fb716a913c7fd
Caio Souza <caio.7s@outlook.com>
2023-01-15 20:27:39 -0300

    feat: adicionando funcionalidade de botões 'keyboard', /help e refatorando métodos de uuid4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
-const { generateUuid1, generateUuid4, generateMultipleUuid4 } = require('./src/uuid');
+const { generateUuid1, generateUuid4 } = require('./src/uuid');
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
@@ -11,23 +11,42 @@ const commands = [
   { command: '/uuid4 {quantidade}', description: 'Gera uma quantidade específica de UUIDs versão 4' },
 ];
 
-bot.setMyCommands(commands);
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  let response = "Lista de comandos disponíveis: \n";
+  commands.forEach(cmd => {
+    response += `${cmd.command} - ${cmd.description}\n`;
+  });
+  bot.sendMessage(chatId, response);
+});
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const keyboard = {
+    keyboard: [
+      [
+        { text: '/uuid1' },
+        { text: '/uuid4' }
+      ],
+      [
+        { text: '/help' }
+      ]
+    ],
+    resize_keyboard: true,
+    one_time_keyboard: true
+  };
+  bot.sendMessage(chatId, 'Selecione uma opção:', { reply_markup: JSON.stringify(keyboard) });
+});
 
 bot.onText(/\/uuid1/, (msg) => {
   const chatId = msg.chat.id;
   const uuid1 = generateUuid1();
-  bot.sendMessage(chatId, uuid1, { parse_mode: "Markdown" });
+  bot.sendMessage(chatId, `\`${uuid1}\``, { parse_mode: 'Markdown' });
 });
 
-bot.onText(/\/uuid4/, (msg) => {
+bot.onText(/\/uuid4( \d+)?/, (msg, match) => {
   const chatId = msg.chat.id;
-  const uuid4 = generateUuid4();
-  bot.sendMessage(chatId, uuid4, { parse_mode: "Markdown" });
-});
-
-bot.onText(/\/uuid4 (\d+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const amount = match[1];
-  const uuids = generateMultipleUuid4(amount);
+  const count = match[1] ? parseInt(match[1]) : 1;
+  let uuids = generateUuid4(count);
   bot.sendMessage(chatId, uuids, { parse_mode: "Markdown" });
 });
